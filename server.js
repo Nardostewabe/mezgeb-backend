cbe744e4858511f5d94b97060be47b32ceae0ebf
Nardos <125884882+Nardostewabe@users.noreply.github.com>
2025-08-17 20:31:43 +0300

    fix file for render

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 
 const admin = require("firebase-admin");
 
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT environment variable is missing!");
+}
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
